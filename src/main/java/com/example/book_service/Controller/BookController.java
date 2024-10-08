ee230ee44f9dbd1ab304a13b8a16dbe76d67dbe2
Goutham-Rajesh <gouthamrajesh5355@gmail.com>
2024-10-08 17:35:33 +0530

    updated

--- src/main/java/com/example/book_service/Controller/BookController.java
+++ src/main/java/com/example/book_service/Controller/BookController.java
@@ -3,6 +3,7 @@ package com.example.book_service.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,6 +34,11 @@ public class BookController {
     @PostMapping("/book")
     public BookEntity addBook(@RequestBody BookEntity bookEntity){
         return bookRepository.saveAndFlush(bookEntity);
+    }
+
+    @DeleteMapping("/book/{id}")
+    public void deleteById(@PathVariable int id){
+        bookRepository.deleteById(id);
     }
     
 
